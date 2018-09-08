8205a9a0906ef71ebc40e46cb66e008e1f1191d1
Shikhar Bakhda <ssb2189@columbia.edu>
2018-09-07 23:54:35 -0400

    done

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
-def main():
-    return render_template('index.html')
-
+@app.route('/pokemon/<query>')
+def main(query='1'):
+    q = requests.request('GET', 'https://www.pokeapi.co/api/v2/pokemon/' + query).json()
+    is_query = query.isdigit()
+    id = q.get('id')
+    name = q.get('name')
+    return render_template('index.html', id=id, name=name, id_query=is_query)
 
 if __name__ == '__main__':
     app.run()
